eb38e5e9dacf64559df4691cf2c9ea0076576afb
Il-Ricci0 <riccardoroman7@gmail.com>
2025-06-28 02:52:46 +0200

    Added basic global styles, started working on signup page.

--- frontend/darTrack/src/app/auth/auth.routes.ts
+++ frontend/darTrack/src/app/auth/auth.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from "@angular/router";
-import { AuthComponent } from "./auth.component";
+import { SigninComponent } from "./signin/signin.component";
+import { SignupComponent } from "./signup/signup.component";
 
 export const authRoutes: Routes = [
     {
-        path: '',
-        component: AuthComponent,
+        path: 'auth/signin',
+        component: SigninComponent,
+    },
+    {
+        path: 'auth/signup',
+        component: SignupComponent,
     }
 ];
---
+++ frontend/darTrack/src/app/auth/signup/signup.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+import { RouterLink } from "@angular/router";
+
+@Component({
+    templateUrl: 'signup.component.html',
+    styleUrl: 'signup.component.scss',
+    imports: [RouterLink]
+})
+export class SignupComponent {
+}
