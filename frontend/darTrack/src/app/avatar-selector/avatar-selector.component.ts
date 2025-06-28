a42e934c326dbec0395bf2b60afb5ea217a1659a
Il-Ricci0 <riccardoroman7@gmail.com>
2025-06-28 05:11:50 +0200

    Updated avatar selector component.

--- frontend/darTrack/src/app/avatar-selector/avatar-selector.component.ts
+++ frontend/darTrack/src/app/avatar-selector/avatar-selector.component.ts
@@ -1,11 +1,17 @@
-import { Component, signal, WritableSignal } from "@angular/core";
+import { AfterViewInit, Component, signal } from "@angular/core";
 
 @Component({
     templateUrl: 'avatar-selector.component.html',
     styleUrl: 'avatar-selector.component.scss',
     selector: 'avatar-selector'
 })
-export class AvatarSelectorComponent {
+export class AvatarSelectorComponent implements AfterViewInit {
+
     selected$ = signal(Math.floor(Math.random() * 6) + 1);
     selecting$ = signal(false);
+    ready$ = signal(false);
+
+    ngAfterViewInit(): void {
+        this.ready$.set(true);
+    }
 }
