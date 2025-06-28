ee456a5927e28fd62fb208b9cdafd53e18a8dd7c
ettig <ciscoettore2002@gmail.com>
2025-06-28 19:24:43 +0200

    feat(app-routes): add new route

--- frontend/darTrack/src/app/app.routes.ts
+++ frontend/darTrack/src/app/app.routes.ts
@@ -1,11 +1,18 @@
 import { Routes } from '@angular/router';
 import { CoreComponent } from './core/core.component';
 import { authRoutes } from './auth/auth.routes';
+import { CreateMatch } from './create-match/create-match';
 
 export const routes: Routes = [
-    {
-        path: '',
-        component: CoreComponent,
-    },
-    ...authRoutes
+  {
+    path: '',
+    component: CoreComponent,
+    children: [
+      {
+        path: 'game/create',
+        component: CreateMatch,
+      },
+    ],
+  },
+  ...authRoutes,
 ];
