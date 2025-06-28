8591a26d08116dcb4d7cb767ae7ff1daaa49cc54
ettig <ciscoettore2002@gmail.com>
2025-06-28 19:23:47 +0200

    feat(create-match): add new component

---
+++ frontend/darTrack/src/app/create-match/create-match.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-match',
+  imports: [],
+  templateUrl: './create-match.html',
+  styleUrl: './create-match.scss'
+})
+export class CreateMatch {
+
+}
