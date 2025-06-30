b1ba480a5b9d514638d72452fa41f1f29175665c
Il-Ricci0 <riccardoroman7@gmail.com>
2025-06-30 23:40:00 +0200

    Added elo enum.

---
+++ frontend/darTrack/src/app/models/enums/elo.enum.ts
@@ -0,0 +1,44 @@
+export enum EElo {
+
+    // =^.^=
+    PlasticI,
+
+    IronIV,
+    IronIII,
+    IronII,
+    IronI,
+
+    BronzeIV,
+    BronzeIII,
+    BronzeII,
+    BronzeI,
+
+    SilverIV,
+    SilverIII,
+    SilverII,
+    SilverI,
+
+    GoldIV,
+    GoldIII,
+    GoldII,
+    GoldI,
+
+    PlatinumIV,
+    PlatinumIII,
+    PlatinumII,
+    PlatinumI,
+
+    EmeraldIV,
+    EmeraldIII,
+    EmeraldII,
+    EmeraldI,
+
+    DiamondIV,
+    DiamondIII,
+    DiamondII,
+    DiamondI,
+
+    Master,
+    GrandMaster,
+    Challenger,
+}
