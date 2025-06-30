dbd5d920248dfc13c2bd4cef2988aaa5c40817dc
Il-Ricci0 <riccardoroman7@gmail.com>
2025-06-30 22:45:33 +0200

    Added game join, game create buttons on dashboard component.

--- frontend/darTrack/src/app/dashboard/dashboard.component.ts
+++ frontend/darTrack/src/app/dashboard/dashboard.component.ts
@@ -1,9 +1,21 @@
-import { Component } from "@angular/core";
+import { Component, signal } from "@angular/core";
+import { EGameContainerStatus } from "../models/enums/game-container-status.enum";
+import { RouterLink } from "@angular/router";
 
 @Component({
     templateUrl: 'dashboard.component.html',
     styleUrl: 'dashboard.component.scss',
-    imports: []
+    imports: [RouterLink]
 })
 export class DashboardComponent {
+    EGameContainerStatus = EGameContainerStatus;
+    gameContainerStatus$ = signal(EGameContainerStatus.Hidden);
+
+    onCasualClick() {
+        this.gameContainerStatus$.update(x => x !== EGameContainerStatus.Casual ? EGameContainerStatus.Casual : EGameContainerStatus.Hidden);
+    }
+
+    onTournamentClick() {
+        this.gameContainerStatus$.update(x => x !== EGameContainerStatus.Tournament ? EGameContainerStatus.Tournament : EGameContainerStatus.Hidden);
+    }
 }
---
+++ frontend/darTrack/src/app/models/enums/game-container-status.enum.ts
@@ -0,0 +1,5 @@
+export enum EGameContainerStatus {
+    Hidden,
+    Casual,
+    Tournament
+}
