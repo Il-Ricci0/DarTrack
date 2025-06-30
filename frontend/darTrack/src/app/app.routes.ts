86818657b6cc54eae22326d8a4e641c50a83a82d
Il-Ricci0 <riccardoroman7@gmail.com>
2025-06-30 22:03:45 +0200

    Added basic style for dashboard component.

--- frontend/darTrack/src/app/app.routes.ts
+++ frontend/darTrack/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { CoreComponent } from './core/core.component';
 import { authRoutes } from './auth/auth.routes';
 import { CreateMatch } from './create-match/create-match';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 export const routes: Routes = [
   {
@@ -12,6 +13,10 @@ export const routes: Routes = [
         path: 'game/create',
         component: CreateMatch,
       },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      }
     ],
   },
   ...authRoutes,
