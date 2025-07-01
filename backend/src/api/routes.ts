0ac850931ba799437c4c0addde3b335a5a5c59dc
TurboGLS <matteazzitomas@gmail.com>
2025-07-01 16:11:25 +0200

    Add user router and controller for user-related endpoints

--- backend/src/api/routes.ts
+++ backend/src/api/routes.ts
@@ -3,6 +3,7 @@ import verificationRouter from './email-verification/verify.router';
 import authRouter from './auth/auth.router';
 import cleanupRouter from './jobs/cleanup.router';
 import eloRouter from './elo/elo.router';
+import userRouter from './user/user.router';
 
 const router = Router();
 
@@ -10,5 +11,6 @@ router.use('/verification', verificationRouter);
 router.use(authRouter);
 router.use('/jobs', cleanupRouter);
 router.use('/elo', eloRouter);
+router.use('/user', userRouter);
 
 export default router;
